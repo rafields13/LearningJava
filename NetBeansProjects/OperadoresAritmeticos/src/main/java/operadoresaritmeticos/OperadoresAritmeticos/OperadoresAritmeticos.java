6dd056e71fafd2953201d2ae47c956081c0a2014
Rafael <rafaelmarliere033@gmail.com>
2023-07-02 14:14:42 -0300

    someTests

---
+++ NetBeansProjects/OperadoresAritmeticos/src/main/java/operadoresaritmeticos/OperadoresAritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operadoresaritmeticos.OperadoresAritmeticos;
+
+/**
+ *
+ * @author rafie
+ */
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.print("A media e igual a " + m);*/
+        
+        /*int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.print(valor);*/
+        
+        /*int x = 4;
+        x *= 2; // x = x * 2
+        System.out.print(x);*/
+        
+        /*float v = 8.6f;
+        int ar = (int) Math.round(v);
+        System.out.print(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50 - 15));
+        System.out.print(n);
+    }
+}
---
+++ NetBeansProjects/SuperCalculadora/src/main/java/SuperCalculadora.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author rafie
+ */
+public class SuperCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
